9ffed7a53ed5930d4f73b0e297f2c16de4196627
VoXzE <sinncere520@gmail.com>
2021-04-10 17:55:28 -0400

    refactor: centered text and removed last child border

--- src/renderer/components/Steps/Steps.tsx
+++ src/renderer/components/Steps/Steps.tsx
@@ -5,13 +5,13 @@ export const Steps = ({ step }: { step: number }) => {
     return (
         <div className={styles.steps}>
             <h1>
-                <span style={{ color: step === 1 && '#F40552' }}>Step 1</span> |
+                <span style={{ color: step === 1 && '#F40552' }}>Step 1</span>
             </h1>
             <h1>
-                <span style={{ color: step === 2 && '#F40552' }}>Step 2</span> |
+                <span style={{ color: step === 2 && '#F40552' }}>Step 2</span>
             </h1>
             <h1>
-                <span style={{ color: step === 3 && '#F40552' }}>Step 3</span> |
+                <span style={{ color: step === 3 && '#F40552' }}>Step 3</span>
             </h1>
             <h1>
                 <span style={{ color: step === 4 && '#F40552' }}>Step 4</span>
