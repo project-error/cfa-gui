dfa1719a1153614d84118da72fd0acc6d77d9a2f
Christopher <onlygamernerd@gmail.com>
2021-04-12 04:33:48 +0200

    remove unused class

--- src/main/classes/template.controller.ts
+++
@@ -1,9 +0,0 @@
-export class Template {
-    readonly templateType: string;
-
-    // fix this for me
-
-    constructor(type: 'TypeScript' | 'JavaScript') {
-        this.templateType = type;
-    }
-}
