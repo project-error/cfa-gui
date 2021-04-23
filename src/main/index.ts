b92cf3939b3224b20fb8563107693443e016ba99
Aleksander Hjortkær Sand Evensen (Elev) <aleksandhe@viken.no>
2021-04-23 13:43:03 +0200

    Setup commander options (not complete)

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,6 +5,63 @@ import './functions/createBoilerplate';
 
 let mainWindow: BrowserWindow | null | undefined;
 
+import { Command } from 'commander';
+
+// Test args, just used for testing the commander tool in dev mode
+const testArgs = ['create', 'TestApp', '-t', '@template/test-app'];
+// Create the commander object
+const cli = new Command();
+
+// Set the version from the package.json file
+cli.version(app.getVersion());
+
+// The create command. Used for creating a resource without using the gui interface.
+cli.command('create <name>')
+    .description('Create a fivem resource using the cli tool')
+    .option(
+        '-t, --template <template_name>',
+        'The template your resource will use',
+        '@cfa/template-lua',
+    )
+    .option('-a, --author <your_name>', 'The name of the author', 'your name')
+    .option(
+        '-v, --version <version>',
+        'The version for your resource',
+        'v0.0.0',
+    )
+    .option(
+        '-d, --desc <description>',
+        'The description of your resource',
+        'My fancy resource',
+    )
+    .action((name, options) => {
+        // console.log('read config from %s', program.opts().config);
+        console.log(cli.opts());
+        console.log(options);
+    });
+
+// The gui command.
+// Used with the context menu action later on
+cli.command('gui <path>').action((name, options) => {
+    console.log(cli.opts());
+    console.log(options);
+});
+
+// In prod mode, there is only 1 default cli arg (cwd)
+// to prevent errors we insert a new element at index 1 in the process.argv array
+if (process.env.MODE === 'production') process.argv.splice(1, 0, '.');
+// process.argv = [...process.argv, ...testArgs];
+
+console.log(process.env.MODE);
+console.log(process.argv);
+
+// To prevent the commander tool to throw an error, thus not starting the electron app
+// we check if there is more cli args than 2 (cwd, entry-file), there for we should use the cli tool
+if (process.argv.length > 2) {
+    cli.parse();
+    console.log(cli.opts());
+}
+
 function createMainWindow(): BrowserWindow {
     const window = new BrowserWindow({
         height: 800,
