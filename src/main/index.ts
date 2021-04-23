30a069aefb3884f7b5780053f35ac58d4c07682b
Aleksander Evensen <ahse03@gmail.com>
2021-04-23 21:34:44 +0200

    Nothing works..... :(

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,5 @@
 import { Command } from 'commander';
+import { app } from 'electron';
 
 // Test args, just used for testing the commander tool in dev mode
 const testArgs = ['create', 'TestApp', '-t', '@template/test-app'];
@@ -27,30 +28,39 @@ cli.command('create <name>')
         'The description of your resource',
         'My fancy resource',
     )
-    .action((name, options) => {
+    .action((name: string, options: any) => {
         // console.log('read config from %s', program.opts().config);
-        console.log(cli.opts());
+        // console.log(cli.opts());
         console.log(options);
     });
 
 // The gui command.
 // Used with the context menu action later on
-cli.command('gui <path>').action((name, options) => {
+cli.command('gui <path>').action((path: string, options: any) => {
     console.log(cli.opts());
     console.log(options);
 });
 
 // In prod mode, there is only 1 default cli arg (cwd)
 // to prevent errors we insert a new element at index 1 in the process.argv array
-if (process.env.MODE === 'production') process.argv.splice(1, 0, '.');
-// process.argv = [...process.argv, ...testArgs];
+// if (process.env.MODE === 'production') process.argv.splice(1, 0, '.');
 
-console.log(process.env.MODE);
+if (process.env.MODE !== 'production')
+    process.argv = [...process.argv, ...testArgs];
 console.log(process.argv);
 
 // To prevent the commander tool to throw an error, thus not starting the electron app
 // we check if there is more cli args than 2 (cwd, entry-file), there for we should use the cli tool
 if (process.argv.length > 2) {
-    cli.parse();
-    console.log(cli.opts());
+    cli.parse(); // parse the cli args
+    app.whenReady().then((): void => {
+        console.log('App Ready');
+        app.quit();
+        app.exit(1);
+        process.exit(1);
+    });
+} else {
+    import('./WinManager').then((): void => {
+        console.log('Init Window');
+    });
 }
