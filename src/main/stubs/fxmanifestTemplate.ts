d6b054fb082099a58c574daa9e70ff252c564655
Christopher <onlygamernerd@gmail.com>
2021-06-01 21:26:20 +0200

    update fxmanifest template

--- src/main/stubs/fxmanifestTemplate.ts
+++ src/main/stubs/fxmanifestTemplate.ts
@@ -10,8 +10,8 @@ author "${project.resourceAuthor}"
 version "${project.resourceVersion}"
 description "${project.resourceDescription}"
             
-client_script "dist/client/*.client.js"
-server_script "dist/server/*.server.js"
+client_script "dist/client/client.js"
+server_script "dist/server/server.js"
             `;
     } else {
         return `
